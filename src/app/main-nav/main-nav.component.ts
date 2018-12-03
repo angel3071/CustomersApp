63093b1f8a6c08e773d0ee05525b40c12c42d30c
Angel Ramírez <aramirez@voicetechsolutions.com.mx>
2018-12-02 18:05:47 -0600

    algunos ajustes after deploy

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,51 +1,59 @@
-import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
-import { OrdersService } from '../services/orders.service';
-import { AngularFireList } from 'angularfire2/database';
-import { Order } from '../models/order';
+import { Component, OnInit } from "@angular/core";
+import { BreakpointObserver, Breakpoints } from "@angular/cdk/layout";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
+import { OrdersService } from "../services/orders.service";
+import { AngularFireList } from "angularfire2/database";
+import { Order } from "../models/order";
 
 @Component({
-  selector: 'app-main-nav',
-  templateUrl: './main-nav.component.html',
-  styleUrls: ['./main-nav.component.css'],
+  selector: "app-main-nav",
+  templateUrl: "./main-nav.component.html",
+  styleUrls: ["./main-nav.component.css"]
 })
-export class MainNavComponent implements OnInit{
-
+export class MainNavComponent implements OnInit {
   headerTitle: string;
   orders: Order[];
-  HEADERS = {entry: 'Entradas', meat: 'Carnes', pizza: 'Pizzas', drink: 'Bebidas', dessert: 'Postres', checkout: 'Su cuenta'}; 
-  route : string;
+  HEADERS = {
+    entry: "Entradas",
+    meat: "Carnes",
+    pizza: "Pizzas",
+    drink: "Bebidas",
+    dessert: "Postres",
+    checkout: "Su cuenta"
+  };
+  route: string;
 
-  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
-    .pipe(
-      map(result => result.matches)
-    );
+  isHandset$: Observable<boolean> = this.breakpointObserver
+    .observe(Breakpoints.Handset)
+    .pipe(map(result => result.matches));
 
-  constructor(private breakpointObserver: BreakpointObserver, private router: Router,
-    private authService: AuthService, private orderService: OrdersService) {
-      this.route = this.router.url.substring(1);
-      this.headerTitle = this.HEADERS[this.route];
-
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private router: Router,
+    public authService: AuthService,
+    private orderService: OrdersService
+  ) {
+    this.route = this.router.url.substring(1);
+    this.headerTitle = this.HEADERS[this.route];
   }
   ngOnInit(): void {
-    this.orderService.getNotifications(this.authService.getUserId())
-    .snapshotChanges()
-    .subscribe(item => {
-      this.orders = [];
-      item.forEach(element => {
-        let x = element.payload.toJSON();
-        x["$key"] = element.key;
-        this.orders.push(x as Order);
+    this.orderService
+      .getNotifications(this.authService.getUserId())
+      .snapshotChanges()
+      .subscribe(item => {
+        this.orders = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.orders.push(x as Order);
+        });
       });
-    });
   }
-  logout(){
-    console.log('Logg out');
+  logout() {
+    console.log("Logg out");
     this.authService.logout();
   }
-
 }
--- src/app/menu-list/menu-list.component.ts
+++ src/app/menu-list/menu-list.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { Plate } from '../models/plate';
-import { MenuItemsService } from '../services/menu-items.service';
-import { MatDialog } from '@angular/material';
-import { MenuItemMoreInfoComponent } from '../menu-item-more-info/menu-item-more-info.component';
-import { MenuItemOrderComponent } from '../menu-item-order/menu-item-order.component';
-import { OrdersService } from '../services/orders.service';
-import { Order } from '../models/order';
-import { Router } from '@angular/router';
-import { AuthService } from '../services/auth.service';
+import { Component, OnInit } from "@angular/core";
+import { Plate } from "../models/plate";
+import { MenuItemsService } from "../services/menu-items.service";
+import { MatDialog } from "@angular/material";
+import { MenuItemMoreInfoComponent } from "../menu-item-more-info/menu-item-more-info.component";
+import { MenuItemOrderComponent } from "../menu-item-order/menu-item-order.component";
+import { OrdersService } from "../services/orders.service";
+import { Order } from "../models/order";
+import { Router } from "@angular/router";
+import { AuthService } from "../services/auth.service";
 
 @Component({
-  selector: 'app-menu-list',
-  templateUrl: './menu-list.component.html',
-  styleUrls: ['./menu-list.component.css']
+  selector: "app-menu-list",
+  templateUrl: "./menu-list.component.html",
+  styleUrls: ["./menu-list.component.css"]
 })
 export class MenuListComponent implements OnInit {
   data: Plate[];
@@ -20,93 +20,94 @@ export class MenuListComponent implements OnInit {
   showSpinner: boolean = true;
   category: string;
 
-  constructor(private menuItemsService: MenuItemsService, 
-    private orderService: OrdersService, private dialog: MatDialog, 
-    private router: Router, private authService: AuthService) { 
-      this.category = this.router.url.substring(1);
-    }
+  constructor(
+    private menuItemsService: MenuItemsService,
+    private orderService: OrdersService,
+    private dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    this.category = this.router.url.substring(1);
+  }
 
   ngOnInit() {
     //console.log('Entering ngOnInit ' + this.data.length);
-    console.log('Actual route ' + this.router.url);
-    console.log('Logged user: ' + this.authService.getUserId());
-    this.menuItemsService.getMenuItems(this.category)
-    .snapshotChanges()
-    .subscribe(item => {
-      this.data = [];
-      item.forEach(element => {
-        let x = element.payload.toJSON();
-        x["$key"] = element.key;
-        this.data.push(x as Plate);
+    console.log("Actual route " + this.router.url);
+    console.log("Logged user: " + this.authService.getUserId());
+    this.menuItemsService
+      .getMenuItems(this.category)
+      .snapshotChanges()
+      .subscribe(item => {
+        this.data = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.data.push(x as Plate);
+        });
+        this.showSpinner = false;
       });
-      this.showSpinner = false;
-    })
-
   }
-  getDisplayName(){ return ''; }
-  getUserId(){ return ''; }
+  getDisplayName() {
+    return "";
+  }
+  getUserId() {
+    return "";
+  }
 
   openDialogMoreInfo(plate: Plate) {
-    const dialogRef = this.dialog.open(MenuItemMoreInfoComponent,
-      {
-        data: {
-          header: plate.name,
-          paragraph: plate.desc
-      },
-      });
-    
+    const dialogRef = this.dialog.open(MenuItemMoreInfoComponent, {
+      data: {
+        header: plate.name,
+        paragraph: plate.desc
+      }
+    });
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result}`);
-      if(result){
+      if (result) {
         console.log(`Plate ordered: ${plate.name}`);
         this.orderService.insertOrder({
           customerName: this.authService.getDisplayName(),
           customerId: this.authService.getUserId(),
-          plate: plate.$key,                                                                                                 
-          plateImage: plate.image,                                                                                            
-          orderNumber: 100,                                                                                          
-          dateTime: new Date().toString(),                                                                                              
-          status: 'ordered',                                                                                               
-          comments: '',
+          plate: plate.$key,
+          plateImage: plate.image,
+          orderNumber: 100,
+          dateTime: new Date().toString(),
+          status: "ordered",
+          comments: "",
           platePrice: plate.price,
           plateName: plate.name
         } as Order);
-      } 
-      
+      }
     });
   }
-  openDialogOrder(plate: Plate){
-    const dialogRef = this.dialog.open(MenuItemOrderComponent,
-      {
-        data: {
-          header: plate.name,
-          paragraph: 'Desea ordenar este platillo?',
-          comment: this.comment
+  openDialogOrder(plate: Plate) {
+    const dialogRef = this.dialog.open(MenuItemOrderComponent, {
+      data: {
+        header: plate.name,
+        paragraph: "Desea ordenar este platillo?",
+        comment: this.comment
       }
-      });
-    
+    });
+
     dialogRef.afterClosed().subscribe(result => {
       console.log(`Dialog result: ${result} ${this.comment}`);
       this.comment = result;
-      if(result!=false){
+      if (result != false) {
         console.log(`Plate ordered: ${plate.name}`);
         this.orderService.insertOrder({
           customerName: this.authService.getDisplayName(),
           customerId: this.authService.getUserId(),
-          plate: plate.$key,                                                                                                 
-          plateImage: plate.image,                                                                                            
-          orderNumber: 100,                                                                                          
-          dateTime: new Date().toString(),                                                                                              
-          status: 'ordered',                                                                                               
+          plate: plate.$key,
+          plateImage: plate.image,
+          orderNumber: 100,
+          dateTime: new Date().toString(),
+          status: "ordered",
           comments: this.comment,
           platePrice: plate.price,
           plateName: plate.name
         } as Order);
-      } 
-      
+      }
     });
-
   }
-
-
 }
